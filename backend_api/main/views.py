af54fc34af72c6c5a4edc218f140700e013d434e
sadhanasapkotaa <mandeyra7@gmail.com>
2024-08-20 11:40:05 +0545

    "View Level Authentication for Vendors"

--- backend_api/main/views.py
+++ backend_api/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from . import models
 from . import serializers
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 # Create your views here.
 class VendorList(generics.ListAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.VendorSerializer
+    permission_classes = [permissions.IsAuthenticated]
